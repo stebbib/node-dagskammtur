e3b1d1ada61404e7209917069079a57ec9ede979
Stefán <stefan@Steb.local>
2015-02-13 17:40:32 +0000

    Initial

---
+++ index.js
@@ -0,0 +1,20 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var tidy = require('htmltidy').tidy;
+
+var getMenu = function() {
+    request('http://dagskammtur.is/matsedill.htm', function(error, res, body) {
+        tidy(body, function(err, html) {
+            var $ = cheerio.load(html);
+            $('p font font').slice(0, 5).each(function(i, e) {
+              var title = $(e).find('b').html();
+              var food = $(e).html().split('<br>\n').slice(1);
+              console.log(title);
+              console.log(food);
+              console.log('----');
+            });
+        });
+    });
+};
+
+modul.exports = getMenu;
