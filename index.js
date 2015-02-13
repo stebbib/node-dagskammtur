e74d7008619b40733a786b3d0de6a5e1ecc2b791
Kristján Oddsson <koddsson@gmail.com>
2015-02-13 20:33:53 +0000

    Call callback once.

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ var getMenu = function(cb) {
     request('http://dagskammtur.is/matsedill.htm', function(error, res, body) {
         tidy(body, function(err, html) {
             var $ = cheerio.load(html);
+            var data = [];
             $('p font font').slice(0, 5).each(function(i, e) {
               var day = $(e).find('b').html();
               var food = $(e).html().split('<br>\n').slice(1);
-              cb({weekday : day, menu_items : food});
+              data.push({weekday : day, menu_items : food});
             });
+            cb(data);
         });
     });
 };
