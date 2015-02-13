26ea0e48f32e8c21a5e3bf13ff5e52acd276db08
Stefán <stefan@Steb.local>
2015-02-13 18:06:55 +0000

    Make it actually return stuff, instead of outputting in console

--- index.js
+++ index.js
@@ -2,16 +2,14 @@ var request = require('request');
 var cheerio = require('cheerio');
 var tidy = require('htmltidy').tidy;
 
-var getMenu = function() {
+var getMenu = function(cb) {
     request('http://dagskammtur.is/matsedill.htm', function(error, res, body) {
         tidy(body, function(err, html) {
             var $ = cheerio.load(html);
             $('p font font').slice(0, 5).each(function(i, e) {
-              var title = $(e).find('b').html();
+              var day = $(e).find('b').html();
               var food = $(e).html().split('<br>\n').slice(1);
-              console.log(title);
-              console.log(food);
-              console.log('----');
+              cb({weekday : day, menu_items : food});
             });
         });
     });
